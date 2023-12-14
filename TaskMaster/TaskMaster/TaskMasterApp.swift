86de50fd89bb737fe11d3bbd1cc9746cd12bb02c
Nick Perkins <nickperkins@protonmail.com>
2023-12-14 13:51:37 -0500

    project commit

---
+++ TaskMaster/TaskMaster/TaskMasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskMasterApp.swift
+//  TaskMaster
+//
+//  Created by Nick Perkins on 12/14/23.
+//
+
+import SwiftUI
+
+@main
+struct TaskMasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
